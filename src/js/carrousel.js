c1febce2afe5fa98966e35e80225fc7331b6de43
Henrique ⠀⠀⠀⠀⠀⠀ <hh1050745@gmail.com>
2024-02-16 23:32:17 -0300

    UP

--- src/js/carrousel.js
+++ src/js/carrousel.js
@@ -1,10 +1,27 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 300,
-    freeMode: true,
-    touchStartPreventDefault: false,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
+var draggableDiv = document.getElementById('draggableDiv');
+    var draggableContent = document.getElementById('draggableContent');
+    var isDown = false;
+    var startX;
+    var scrollLeft;
+
+    draggableDiv.addEventListener('mousedown', function(e) {
+        isDown = true;
+        draggableDiv.style.cursor = 'grabbing';
+        startX = e.pageX - draggableDiv.offsetLeft;
+        scrollLeft = draggableDiv.scrollLeft;
+    });
+    draggableDiv.addEventListener('mouseleave', function() {
+        isDown = false;
+        draggableDiv.style.cursor = 'grab';
+    });
+    draggableDiv.addEventListener('mouseup', function() {
+        isDown = false;
+        draggableDiv.style.cursor = 'grab';
+    });
+    draggableDiv.addEventListener('mousemove', function(e) {
+        if(!isDown) return;
+        e.preventDefault();
+        var x = e.pageX - draggableDiv.offsetLeft;
+        var walk = (x - startX) * 3; 
+        draggableDiv.scrollLeft = scrollLeft - walk;
+    });
