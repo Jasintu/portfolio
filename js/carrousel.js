46c683dfa8ff9071d0e38767f2b4e39db5120697
Jasintu <hh1050745@gmail.com@email.com>
2024-03-12 18:12:30 -0300

    UP

--- js/carrousel.js
+++ js/carrousel.js
@@ -26,6 +26,34 @@ var draggableDiv = document.getElementById('draggableDiv');
         draggableDiv.scrollLeft = scrollLeft - walk;
     });
 
+var draggableDiv2 = document.getElementById('projects');
+    var draggableContent2 = document.getElementById('proj');
+    var isDown = false;
+    var startX;
+    var scrollLeft;
+
+    draggableDiv2.addEventListener('mousedown', function(e) {
+        isDown = true;
+        draggableDiv2.style.cursor = 'grabbing';
+        startX = e.pageX - draggableDiv2.offsetLeft;
+        scrollLeft = draggableDiv2.scrollLeft;
+    });
+    draggableDiv2.addEventListener('mouseleave', function() {
+        isDown = false;
+        draggableDiv2.style.cursor = 'grab';
+    });
+    draggableDiv2.addEventListener('mouseup', function() {
+        isDown = false;
+        draggableDiv2.style.cursor = 'grab';
+    });
+    draggableDiv2.addEventListener('mousemove', function(e) {
+        if(!isDown) return;
+        e.preventDefault();
+        var x = e.pageX - draggableDiv2.offsetLeft;
+        var walk = (x - startX) * 1; 
+        draggableDiv2.scrollLeft = scrollLeft - walk;
+    });
+
     
     var draggableDiv = document.getElementById('draggableDiv');
     var isDragging = false;
@@ -54,6 +82,12 @@ var draggableDiv = document.getElementById('draggableDiv');
     let minhaDiv = document.getElementById('draggableDiv');
 
     minhaDiv.addEventListener('touchstart', function(event) {
+        event.stopPropagation();
+    });
+
+    let minhaDiv2 = document.getElementById('projects');
+
+    minhaDiv2.addEventListener('touchstart', function(event) {
         event.stopPropagation();
     });
     
